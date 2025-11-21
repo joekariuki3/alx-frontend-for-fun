711ae2d5578e04d65ef347c82ed7fc17f0d3aa6f
joekariuki3 <joekariuki3@gmail.com>
2025-11-21 09:24:59 +0300

    refactor: move imports inside the main block for better encapsulation

--- markdown2html.py
+++ markdown2html.py
@@ -4,16 +4,17 @@
 Script that takes two arguments, markdown_file_name, output_file_name
 """
 
-import os
-import sys
+if __name__ == "__main__":
+    import os
+    import sys
 
-argc = len(sys.argv)
-if argc < 2:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html \n")
-    exit(1)
-elif argc >= 2:
-    markdown_file_name = sys.argv[1]
-
-    if not os.path.exists(markdown_file_name):
-        sys.stderr.write(f"Missing {markdown_file_name}\n")
+    argc = len(sys.argv)
+    if argc < 2:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html \n")
         exit(1)
+    elif argc >= 2:
+        markdown_file_name = sys.argv[1]
+
+        if not os.path.exists(markdown_file_name):
+            sys.stderr.write(f"Missing {markdown_file_name}\n")
+            exit(1)

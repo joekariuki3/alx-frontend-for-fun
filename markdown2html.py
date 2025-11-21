916e6f345cf95017869740f1506679d155c55245
joekariuki3 <joekariuki3@gmail.com>
2025-11-21 09:13:27 +0300

    add markdown2html script for converting markdown to HTML

---
+++ markdown2html.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+"""
+Script that takes two arguments, markdown_file_name, output_file_name
+"""
+
+import os
+import sys
+
+argc = len(sys.argv)
+if argc < 2:
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html \n")
+    exit(1)
+elif argc >= 2:
+    markdown_file_name = sys.argv[1]
+
+    if not os.path.exists(markdown_file_name):
+        sys.stderr.write(f"Missing {markdown_file_name}\n")
+        exit(1)
